7f67c242dbe8e1e490bf913339437ccd49f63896
eduardohfnascimento <eduardo.h.f@hotmail.com>
2018-04-14 02:33:07 -0300

    1069 problem

---
+++ uri/1069.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include<stack>
+using namespace std;
+
+int main() {
+	int a;
+	while (cin >> a)
+	{
+		for (int index = 0; index < a; ++index)
+		{
+			int result = 0;
+			string str;
+			stack<char> q;
+			cin >> str;
+			for (int index1 = 0; index1 < str.size(); ++index1)
+			{
+				if (str[index1] != '.')
+				{
+					if (!q.empty() && str[index1] == '>')
+					{
+						if (q.top() == '<')
+						{
+							q.pop();
+							result = result + 1;
+						}
+
+					}
+					else
+					{
+						q.push(str[index1]);
+					}
+				}
+
+			}
+			cout << result << endl;
+		}
+
+	}
+}
