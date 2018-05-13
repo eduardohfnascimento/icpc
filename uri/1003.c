143a9c3a4048d8145a5e709b78bc7327cdfddb64
eduardohfnascimento <eduardo.h.f@hotmail.com>
2018-05-13 15:51:08 -0300

    add 1003

---
+++ uri/1003.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int raio;
+	int pi = 3.14159;
+	scanf(" %d", &raio);
+	scanf(" %d", &pi);
+
+	int X  = raio+pi;
+
+	printf("SOMA = %d\n", X);
+ 
+    return 0;
+}
