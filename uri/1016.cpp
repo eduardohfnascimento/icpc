bad68a70f60e995b0c18871548a4424995505fc0
eduardohfnascimento <eduardo.h.f@hotmail.com>
2018-05-31 03:53:36 -0300

    1016

---
+++ uri/1016.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int kilometros;
+
+    cin >> kilometros;
+    int minutos = kilometros * 2;
+
+    cout << minutos << " minutos" << endl;
+
+    return 0;
+}
